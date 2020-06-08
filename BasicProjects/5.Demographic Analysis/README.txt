f790c1c9630cb1916811194943dd9ffb467a8301
chintanbhargave <chintanbhargave@gmail.com>
2020-06-08 14:46:14 +0530

    challenge2 completed

--- BasicProjects/5.Demographic Analysis/README.txt
+++ BasicProjects/5.Demographic Analysis/README.txt
@@ -2,10 +2,14 @@ Problem Statement :
 
 You are employed as a Data Scientist by the World Bank and you are working on a project to analyse the World's demographic trends.
 
-You are required to produce a scatterplot illustrating Birth Rate and Internet Usage statistic by Country.
+1.You are required to produce a scatterplot illustrating Birth Rate and Internet Usage statistic by Country.
 
 The scatterplot needs to also be categorised by Countries' Income groups.
-
 The data is provided into DemographicData.csv.
 
-The data frames are used in this project and some basic operations on data frames are performed for practice.
+2.You have received and urgent update from your manager.
+You are required to produce a scatterplot illustrating Birth Rate and Internet Usage statistic by Country.
+
+However, this time the scatterplot need to be categorised by Countries' Regions.
+
+Additional data has been in the form of R vectors in CountryRegionVectors.R file.
--- BasicProjects/5.Demographic Analysis/code.R
+++ BasicProjects/5.Demographic Analysis/code.R
@@ -1,7 +1,8 @@
+
 demo<- read.csv("DemographicData.csv", stringsAsFactors = T)
 demo
 
-# basic operations 
+# basic operations on data frame for practice.
 nrow(demo)
 ncol(demo)
 head(demo,n = 20)
@@ -21,12 +22,48 @@ demo$abcd <- NULL
 
 demo[demo$Country.Name == "Malta",]
 
-# plotting
+# plotting practice
 qplot(data = demo, x=Internet.users)
 
 qplot(data = demo, x=Income.Group, y = Birth.rate)
 
 qplot(data = demo, x = Internet.users, y = Birth.rate, color = I("red"), size = I(3))
 
-
+# challenge 1
 qplot(data = demo, x = Internet.users, y = Birth.rate, color = Income.Group, size = I(3))
+
+# challenge 2
+
+# creating data frames for vectors
+df <- data.frame(Country=Countries_2012_Dataset,Codes=Codes_2012_Dataset,Regions=Regions_2012_Dataset)
+#colnames(df) <- c("Country","Codes","Regions")
+
+#  Merging data frames
+mdf <- merge(demo,df,by.x = "Country.Code", by.y = "Codes")
+mdf$Country <- NULL
+head(mdf)
+str(mdf)
+summary(mdf)
+
+
+# challenge 2 plot
+
+qplot(data = mdf, x = Internet.users, y = Birth.rate,
+      color = Regions, size = I(3),shape = I(19),alpha=I(0.6),
+      main = "BirthRate vs InternetUsers")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
